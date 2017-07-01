28a31626c143cf75c6023e12f697c1174871f356
mattcorrente <mcorrente72@gmail.com>
2017-07-01 15:28:41 -0700

    reworked plot to make a histogram and output basic stats to console

--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,9 @@
 import Hash
 import matplotlib.pyplot as plt
 import time
+import pandas as pd
 
 def get_repeat_plot(fileName, radix,modulus):
-	radix = 128
 	n =  0
 
 	file = open(fileName)
@@ -32,12 +32,60 @@ def get_repeat_plot(fileName, radix,modulus):
 	repeats = []
 	for i in range(0, len(table)-1):
 		indices.append(i)
-		repeats.append(len(table[i])) # get chain size 
+		repeats.append(len(table[i])) # get chain size
 	
-	return indices, repeats, len(words) 
+	return indices, repeats, len(words)
+
+def get_all_hash_values(fileName, radix,modulus):
+	file = open(fileName)
+	lines = file.read().strip().split('\n')
+	words = []
+	for line in lines:
+		for word in line.strip().split(' '):
+			if word != '':
+				words.append(word)
+
+	hashList = []
+
+	for word in words:
+		hash = int(Hash.hash(word, radix, modulus))
+		hashList.append(hash)
+
+	return hashList
 
 
-def test_modulus_radix_pairs(fileName, outFileName, moduli, radices): 
+def test_modulus_radix_pairs_hist(fileName, outFileName, moduli, radices):
+	plt.figure(figsize=(20, 8))
+	max_repeat = 0
+	plot_num = 1
+	for modulus in moduli:
+		for radix in radices:
+			hashes = get_all_hash_values(fileName, radix, modulus)
+			indices, repeats, key_count = get_repeat_plot(fileName, radix, modulus)
+			zeros = 0
+			for r in repeats:
+				if r == 0:
+						zeros += 1
+				if r > max_repeat:
+					max_repeat = r
+			ax = plt.subplot(len(moduli), len(radices), plot_num)
+			if plot_num == 1: # title on first plot
+				ax.set_title("Key Clustering for " + fileName + " with " + str(key_count) + " Keys ")
+			ax.hist(hashes, bins = modulus, histtype='step', label='Radix:' + str(radix) + '/Modulus:'  + str(modulus))
+			ax.legend(loc='upper left')
+			ax.set_ylabel('Frequency')
+			ax.set_xlabel('Table Index')
+			plot_num += 1
+			s = pd.Series(hashes)
+			print s.describe()
+			print 'number of empty locations: ' + str(zeros)
+			print 'number of singles: ' + str(repeats.count(1))
+			print(' ')
+
+	plt.savefig(outFileName)
+	plt.show()
+
+def test_modulus_radix_pairs(fileName, outFileName, moduli, radices):
 	plt.figure(figsize=(20, 8))
 	max_repeat = 0
 	plot_num = 1
@@ -48,30 +96,37 @@ def test_modulus_radix_pairs(fileName, outFileName, moduli, radices):
 				if r > max_repeat:
 					max_repeat = r
 			ax = plt.subplot(len(moduli), len(radices), plot_num)
-			if plot_num == 1: # title on first plot
+			if plot_num == 1:  # title on first plot
 				ax.set_title("Key Clustering for " + fileName + " with " + str(key_count) + " Keys ")
 
-			#ax.bar(indices, repeats, width)
-			ax.plot(indices, repeats,'-o', label='Radix:' + str(radix) + '/Modulus:'  + str(modulus))
+			# ax.bar(indices, repeats, width)
+			ax.plot(indices, repeats, '-o', label='Radix:' + str(radix) + '/Modulus:' + str(modulus))
 			ax.legend(loc='upper left')
 			plot_num += 1
-		
+
 	ax.set_ylabel('Key count')
 	ax.set_xlabel('Table Index')
 	plt.savefig(outFileName)
 	plt.show()
 
-radices = [128]
-moduli = [32, 127, 97]
-test_modulus_radix_pairs('dataFiles/5lw-s.dat', 'plots/5lw-s-plot.png', moduli, radices)
-test_modulus_radix_pairs('dataFiles/wordList.txt', 'plots/wordList.png', moduli, radices)
+def plot_histogram_hash(fileName, outFileName, moduli, radices):
+	plt.figure(figsize=(20, 8))
+	plot_num = 1
+	for modulus in moduli:
+		for radix in radices:
+			hashes = get_all_hash_values(fileName, radix, modulus)
+			ax = plt.subplot(len(moduli), len(radices), plot_num)
+			if plot_num == 1:  # title on first plot
+				ax.set_title("Key Clustering for " + fileName + " with " + str(len(hashes)) + " Keys ")
+			ax.hist(hashes)
+			ax.legend(loc='upper left')
+			plot_num += 1
 
-radices = [256]
-moduli = [50, 500, 5000]
-test_modulus_radix_pairs('dataFiles/5lw-m.dat', 'plots/5lw-m-plot.png', moduli, radices)
+	ax.set_ylabel('Key count')
+	ax.set_xlabel('Table Index')
+	plt.savefig(outFileName)
+	plt.show()
 
-
-
-radices = [256]
-moduli = [100, 1000, 10000]
-test_modulus_radix_pairs('dataFiles/5lw.dat', 'plots/5lw-plot.png', moduli, radices)
+radices = [128, 256]
+moduli = [127, 123, 213, 255]
+test_modulus_radix_pairs_hist('../dataFiles/5lw-m.dat', '../plots/5lw-m-plot2.png', moduli, radices)
